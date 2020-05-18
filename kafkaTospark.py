2e62dcc69865969477108bfc0cad7e9eae9790a6
indirameduri <45184206+indirameduri@users.noreply.github.com>
2020-05-18 12:43:52 -0500

    changes

--- kafkaTospark.py
+++ kafkaTospark.py
@@ -11,7 +11,7 @@ import os
 
 #BenefitCostSharing 1
 
-def BenefitsCostSharing1():
+def benefits_cost_sharing1():
   
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
     
@@ -19,17 +19,17 @@ def BenefitsCostSharing1():
     #Topics transfer
     spark = SparkSession.builder.getOrCreate()
     df1 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","BenefitsCostSharing1").option("startingOffsets","earliest").load()
-    BenefitCS_df1 = df1.selectExpr("CAST(value AS STRING)")
-    output_query = BenefitCS_df1.writeStream.queryName("Benefits1").format("memory").start()
+    benefitCS_df1 = df1.selectExpr("CAST(value AS STRING)")
+    output_query = benefitCS_df1.writeStream.queryName("benefits1").format("memory").start()
     output_query.awaitTermination(10)
     #output_query = BenefitCS_df1.writeStream.format("console").start()
     
-    BenefitCSdf1 = spark.sql('select * from Benefits1')
-    BenefitCSdf1.show(2)
+    benefitCSdf1 = spark.sql('select * from benefits1')
+    benefitCSdf1.show(2)
     
-    BenefitCS_rdd1 = BenefitCSdf1.rdd.map(lambda i: i['value'].split('\t'))
+    benefitCS_rdd1 = benefitCSdf1.rdd.map(lambda i: i['value'].split('\t'))
     
-    Benefits_row_rdd1 = BenefitCS_rdd1.map(lambda i: Row(BenefitName = i[0],\
+    benefits_row_rdd1 = benefitCS_rdd1.map(lambda i: Row(BenefitName = i[0],\
                                                        BusinessYear = i[1],\
                                                        EHBVarReason = i[2],\
                                                        IsCovered = i[3],\
@@ -41,35 +41,35 @@ def BenefitsCostSharing1():
                                                        SourceName = i[9],\
                                                        StateCode = i[10]))
      
-    BDF1 = spark.createDataFrame(Benefits_row_rdd1)
-    BDF1.show(2)
+    bdf1 = spark.createDataFrame(Benefits_row_rdd1)
+    bdf1.show(2)
     #Spark to Mongo
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    BDF1.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
+    bdf1.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
     print('\nData tranfered from Spark to Mongo!!')
     spark.stop() 
 
 
 #BenefitCostSharing2 Topic
-def BenefitsCostSharing2():
+def benefits_cost_sharing2():
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
     
     print('Kafka->Spark-BenefitsCostSharing 2 ')
     #Topics transfer
     spark = SparkSession.builder.getOrCreate()
     df2 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","BenefitsCostSharing2").option("startingOffsets","earliest").load()
-    BenefitCS_df2 = df2.selectExpr("CAST(value AS STRING)")
-    output_query = BenefitCS_df2.writeStream.queryName("Benefits2").format("memory").start()
+    benefitCS_df2 = df2.selectExpr("CAST(value AS STRING)")
+    output_query = benefitCS_df2.writeStream.queryName("benefits2").format("memory").start()
     output_query.awaitTermination(25)
     #output_query = BenefitCS_df1.writeStream.format("console").start()
     
-    BenefitCSdf2 = spark.sql('select * from Benefits2')
+    benefitCSdf2 = spark.sql('select * from benefits2')
     
     
-    BenefitCS_rdd2 = BenefitCSdf2.rdd.map(lambda i: i['value'].split('\t'))
+    benefitCS_rdd2 = benefitCSdf2.rdd.map(lambda i: i['value'].split('\t'))
     
-    Benefits_row_rdd2 = BenefitCS_rdd2.map(lambda i: Row(BenefitName = i[0],\
+    benefits_row_rdd2 = benefitCS_rdd2.map(lambda i: Row(BenefitName = i[0],\
                                                        BusinessYear = i[1],\
                                                        EHBVarReason = i[2],\
                                                        IsCovered = i[3],\
@@ -81,18 +81,18 @@ def BenefitsCostSharing2():
                                                        SourceName = i[9],\
                                                        StateCode = i[10]))
      
-    BDF2 = spark.createDataFrame(Benefits_row_rdd2)
-    BDF2.show(2)
+    bdf2 = spark.createDataFrame(Benefits_row_rdd2)
+    bdf2.show(2)
     #Spark to Mongo
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    BDF2.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
+    bDF2.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
     print('\nData tranfered from Spark to Mongo!!')
     spark.stop()
     
 #BenefitsCostSharing 3
 
-def BenefitsCostSharing3():
+def benefits_cost_sharing3():
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
     
@@ -100,20 +100,20 @@ def BenefitsCostSharing3():
     #Topics transfer
     spark = SparkSession.builder.getOrCreate()
     df3 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","BenefitsCostSharing3").option("startingOffsets","earliest").load()
-    BenefitCS_df3 = df3.selectExpr("CAST(value AS STRING)")
+    benefitCS_df3 = df3.selectExpr("CAST(value AS STRING)")
     print('BenefitsCostSharing3')
 
 
-    output_query = BenefitCS_df3.writeStream.queryName("Benefits3").format("memory").start()
+    output_query = benefitCS_df3.writeStream.queryName("benefits3").format("memory").start()
     output_query.awaitTermination(25)
     #output_query = BenefitCS_df1.writeStream.format("console").start()
     
-    BenefitCSdf3 = spark.sql('select * from Benefits3')
+    benefitCSdf3 = spark.sql('select * from benefits3')
     
     
-    BenefitCS_rdd3 = BenefitCSdf3.rdd.map(lambda i: i['value'].split('\t'))
+    benefitCS_rdd3 = benefitCSdf3.rdd.map(lambda i: i['value'].split('\t'))
     
-    Benefits_row_rdd3 = BenefitCS_rdd3.map(lambda i: Row(BenefitName = i[0],\
+    benefits_row_rdd3 = benefitCS_rdd3.map(lambda i: Row(BenefitName = i[0],\
                                                        BusinessYear = i[1],\
                                                        EHBVarReason = i[2],\
                                                        IsCovered = i[3],\
@@ -125,16 +125,16 @@ def BenefitsCostSharing3():
                                                        SourceName = i[9],\
                                                        StateCode = i[10]))
      
-    BDF3 = spark.createDataFrame(Benefits_row_rdd3)
-    BDF3.show(2)
+    bdf3 = spark.createDataFrame(Benefits_row_rdd3)
+    bdf3.show(2)
     #Spark to Mongo
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    BDF3.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
+    bdf3.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
     print('\nData tranfered from Spark to Mongo!!')
     spark.stop()
     
-def BenefitsCostSharing4():  
+def benefits_cost_sharing4():  
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
     
@@ -143,18 +143,16 @@ def BenefitsCostSharing4():
     spark = SparkSession.builder.getOrCreate()
     
     df4 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","BenefitsCostSharing4").option("startingOffsets","earliest").load()
-    BenefitCS_df4 = df4.selectExpr("CAST(value AS STRING)")
+    benefitCS_df4 = df4.selectExpr("CAST(value AS STRING)")
     #BenefitsCostSharing 4
-    output_query = BenefitCS_df4.writeStream.queryName("Benefits4").format("memory").start()
+    output_query = benefitCS_df4.writeStream.queryName("benefits4").format("memory").start()
     output_query.awaitTermination(10)
+       
+    benefitCSdf4 = spark.sql('select * from benefits4')
+      
+    benefitCS_rdd4 = benefitCSdf4.rdd.map(lambda i: i['value'].split('\t'))
     
-    
-    BenefitCSdf4 = spark.sql('select * from Benefits4')
-    
-    
-    BenefitCS_rdd4 = BenefitCSdf4.rdd.map(lambda i: i['value'].split('\t'))
-    
-    Benefits_row_rdd4 = BenefitCS_rdd4.map(lambda i: Row(BenefitName = i[0],\
+    benefits_row_rdd4 = benefitCS_rdd4.map(lambda i: Row(BenefitName = i[0],\
                                                        BusinessYear = i[1],\
                                                        EHBVarReason = i[2],\
                                                        IsCovered = i[3],\
@@ -166,17 +164,17 @@ def BenefitsCostSharing4():
                                                        SourceName = i[9],\
                                                        StateCode = i[10]))
      
-    BDF4 = spark.createDataFrame(Benefits_row_rdd4)
-    BDF4.show(5)
+    bdfF4 = spark.createDataFrame(Benefits_row_rdd4)
+    bdf4.show(5)
     #Spark to Mongo
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    BDF4.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
+    bdf4.write.format('com.mongodb.spark.sql.DefaultSource').mode('append').option('database','healthInsurance').option('collection','BenefitsCostSharing').save()   
     print('\nData tranfered from Spark to Mongo!!')
     spark.stop()
 
 #Network Topic
-def Network():
+def network():
     
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
     
@@ -185,18 +183,18 @@ def Network():
     #Topics transfer
     spark = SparkSession.builder.getOrCreate()
     df = spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","Network").option("startingOffsets","earliest").load()
-    Network_df = df.selectExpr("CAST(value AS STRING)")
+    network_df = df.selectExpr("CAST(value AS STRING)")
     print('\nNetwork Topic transfered to Spark')
     
-    output_query = Network_df.writeStream.queryName("Network").format("memory").start()
+    output_query = network_df.writeStream.queryName("Network").format("memory").start()
     output_query.awaitTermination(10)
    
     
-    Networkdf = spark.sql('select * from Network')
+    networkdf = spark.sql('select * from Network')
     
     
-    Network_rdd = Networkdf.rdd.map(lambda i: i['value'].split(','))
-    Network_row_rdd = Network_rdd.map(lambda i: Row(BusinessYear = i[0],\
+    network_rdd = networkdf.rdd.map(lambda i: i['value'].split(','))
+    network_row_rdd = network_rdd.map(lambda i: Row(BusinessYear = i[0],\
                                                     StateCode = i[1],\
                                                     IssuerId = i[2],\
                                                     SourceName = i[3],\
@@ -211,17 +209,17 @@ def Network():
                                                     MarketCoverage= i[12],
                                                     DentalOnlyPlan = i[13]))
     
-    NDF = spark.createDataFrame(Network_row_rdd)
-    NDF.show(5)
+    ndf = spark.createDataFrame(Network_row_rdd)
+    ndf.show(5)
     
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    NDF.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','Network').save()
+    ndf.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','Network').save()
     print('Network transferred to MongoDB')
     spark.stop()
     
 #ServiceArea Topic
-def ServiceArea():
+def service_area():
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
     
@@ -230,17 +228,15 @@ def ServiceArea():
     spark = SparkSession.builder.getOrCreate()
     
     df = spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","ServiceArea").option("startingOffsets","earliest").load()
-    ServiceA_df = df.selectExpr("CAST(value AS STRING)")
+    serviceA_df = df.selectExpr("CAST(value AS STRING)")
     
-    output_query = ServiceA_df.writeStream.queryName("ServiceArea").format("memory").start()
+    output_query =serviceA_df.writeStream.queryName("ServiceArea").format("memory").start()
     output_query.awaitTermination(10)
+       
+    serviceA_df = spark.sql('select * from ServiceArea')
     
-    
-    ServiceA_df = spark.sql('select * from ServiceArea')
-    
-    
-    ServiceA_rdd = ServiceA_df.rdd.map(lambda i: i['value'].split(','))
-    ServiceA_row_rdd = ServiceA_rdd.map(lambda i: Row(BusinessYear = i[0],\
+    serviceA_rdd = serviceA_df.rdd.map(lambda i: i['value'].split(','))
+    serviceA_row_rdd = serviceA_rdd.map(lambda i: Row(BusinessYear = i[0],\
                                                       StateCode= i[1],\
                                                       IssuerId = i[2],\
                                                       SourceName = i[3],\
@@ -260,17 +256,17 @@ def ServiceArea():
                                                       DentalOnlyPlan = i[17]))
     
          
-    SADF = spark.createDataFrame(ServiceA_row_rdd)
-    SADF.show(5)
+    sadf = spark.createDataFrame(ServiceA_row_rdd)
+    sadf.show(5)
        
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    SADF.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','ServiceArea').save()
+    sadf.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','ServiceArea').save()
     print('\nServiceArea Topic transfered to Spark')
     spark.stop()
     
 #Insurance Topic
-def Insurance():
+def insurance():
     
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
@@ -280,17 +276,17 @@ def Insurance():
     spark = SparkSession.builder.master('local[*]').getOrCreate()
     
     df = spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","Insurance").option("startingOffsets","earliest").load()
-    Insurance_df = df.selectExpr("CAST(value AS STRING)")
+    insurance_df = df.selectExpr("CAST(value AS STRING)")
     print('\nInsurance Topic transfered to Spark')
     
-    output_query = Insurance_df.writeStream.queryName("Insurance").format("memory").start()
+    output_query = insurance_df.writeStream.queryName("Insurance").format("memory").start()
     output_query.awaitTermination(10)
     
     
-    Insurance_df = spark.sql('select * from Insurance')
+    insurance_df = spark.sql('select * from Insurance')
     
-    Insurance_rdd = Insurance_df.rdd.map(lambda i: i['value'].split('\t'))
-    Insurance_row_rdd = Insurance_rdd.map(lambda i: Row(age = i[0],\
+    insurance_rdd = insurance_df.rdd.map(lambda i: i['value'].split('\t'))
+    insurance_row_rdd = insurance_rdd.map(lambda i: Row(age = i[0],\
                                                        sex = i[1],\
                                                        bmi = i[2],\
                                                        children = i[3],\
@@ -299,17 +295,17 @@ def Insurance():
                                                        charges = i[6]))
     
          
-    IDF = spark.createDataFrame(Insurance_row_rdd)
-    IDF.show(5)
+    idf = spark.createDataFrame(Insurance_row_rdd)
+    idf.show(5)
        
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    IDF.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','Insurance').save()
+    idf.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','Insurance').save()
     print('Insurance Data transferred to MongoDB')
     spark.stop()
  
 #Plan Attribute Topic
-def Plan_Attribute():
+def plan_attribute():
     
     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 pyspark-shell'
 
@@ -319,17 +315,17 @@ def Plan_Attribute():
     spark = SparkSession.builder.getOrCreate()
     
     df = spark.readStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").option("subscribe","Plan_Attribute").option("startingOffsets","earliest").load()
-    PlanA_df = df.selectExpr("CAST(value AS STRING)")
+    planA_df = df.selectExpr("CAST(value AS STRING)")
     print('\nPlanAttribute Topic Transfered to spark')
     
-    output_query = PlanA_df.writeStream.queryName("PlanA").format("memory").start()
+    output_query = planA_df.writeStream.queryName("PlanA").format("memory").start()
     output_query.awaitTermination(10)
     
     
-    PlanA_df = spark.sql('select * from PlanA')
+    planA_df = spark.sql('select * from PlanA')
     
-    PlanA_rdd = PlanA_df.rdd.map(lambda i: i['value'].split('\t'))
-    PlanA_row_rdd = PlanA_rdd.map(lambda i: Row(AttributesID=i[0], \
+    planA_rdd = planA_df.rdd.map(lambda i: i['value'].split('\t'))
+    planA_row_rdd = planA_rdd.map(lambda i: Row(AttributesID=i[0], \
                                                BeginPrimaryCareCostSharingAfterNumberOfVisits=i[1], \
                                                BeginPrimaryCareDeductibleCoinsuranceAfterNumberOfCopays=i[2], \
                                                BenefitPackageId=i[3], \
@@ -374,11 +370,11 @@ def Plan_Attribute():
                                                WellnessProgramOffered=i[42]))
     
          
-    PADF = spark.createDataFrame(PlanA_row_rdd)
-    PADF.show(5)
+    padf = spark.createDataFrame(PlanA_row_rdd)
+    padf.show(5)
        
     uri = 'mongodb://localhost/healthInsurance.dbs'
     spark_mongodb = SparkSession.builder.config('spark.mongodb.input.uri',uri).config('spark.mongodb.output.uri',uri).getOrCreate()
-    PADF.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','PlanAttribute').save()
+    padf.write.format('com.mongodb.spark.sql.DefaultSource').mode('overwrite').option('database','healthInsurance').option('collection','PlanAttribute').save()
     print('Plan Attribute Data transferred to MongoDB')
     spark.stop()
